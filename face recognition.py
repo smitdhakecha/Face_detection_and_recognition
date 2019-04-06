07642cc6e9feb9db44f5ab4cdd4ab29461d8a918
Smit Dhakecha <34363117+smitdhakecha@users.noreply.github.com>
2019-04-06 18:06:43 +0530

    Add files via upload

---
+++ face recognition.py
@@ -0,0 +1,188 @@
+from imutils import paths
+import numpy as np
+import imutils
+import pickle
+import cv2
+import os
+
+
+protoPath = os.path.join('C:\\Users\\kkkkkkkkkkk\\Downloads\\opencv-face-recognition\\face_detection_model', "deploy.prototxt")
+modelPath = os.path.join('C:\\Users\\kkkkkkkkkkk\\Downloads\\opencv-face-recognition\\face_detection_model',
+	"res10_300x300_ssd_iter_140000.caffemodel")
+detector = cv2.dnn.readNetFromCaffe(protoPath, modelPath)
+
+embedder = cv2.dnn.readNetFromTorch('C:\\Users\\kkkkkkkkkkk\\Downloads\\opencv-face-recognition\\openface_nn4.small2.v1.t7')
+
+
+print("[INFO] quantifying faces...")
+imagePaths = list(paths.list_images('C:\\Users\\kkkkkkkkkkk\\Downloads\\opencv-face-recognition\\dataset'))
+
+knownEmbeddings = []
+knownNames = []
+
+# initialize the total number of faces processed
+total = 0
+
+# loop over the all the images
+for (i, imagePath) in enumerate(imagePaths):
+	name = imagePath.split(os.path.sep)[-2]
+
+	# load the image, resize it to have a width of 600 pixels (while
+	# maintaining the aspect ratio), and then grab the image
+	# dimensions
+	image = cv2.imread(imagePath)
+	image = imutils.resize(image, width=600)
+	(h, w) = image.shape[:2]
+
+	# construct a blob from the image
+	imageBlob = cv2.dnn.blobFromImage(
+		cv2.resize(image, (300, 300)), 1.0, (300, 300),
+		(104.0, 177.0, 123.0), swapRB=False, crop=False)
+
+	# apply OpenCV's deep learning-based face detector to localize
+	# faces in the input image
+	detector.setInput(imageBlob)
+	detections = detector.forward()
+
+	# ensure at least one face was found
+	if len(detections) > 0:
+		# we're making the assumption that each image has only ONE
+		# face, so find the bounding box with the largest probability
+		i = np.argmax(detections[0, 0, :, 2])
+		confidence = detections[0, 0, i, 2]
+
+		# ensure that the detectionwith the largest probability also
+		# means our minimum probability test (thus helping filter out
+		# weak detections)
+		if confidence > 0.6:
+			# compute the (x, y)-coordinates of the bounding box for
+			# the face
+			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+			(startX, startY, endX, endY) = box.astype("int")
+
+			# extract the face ROI and grab the ROI dimensions
+			face = image[startY:endY, startX:endX]
+			(fH, fW) = face.shape[:2]
+
+			# ensure the face width and height are sufficiently large
+			if fW < 20 or fH < 20:
+				continue
+
+			# construct a blob for the face ROI, then pass the blob
+			# through our face embedding model to obtain the 128-d
+			# quantification of the face
+			faceBlob = cv2.dnn.blobFromImage(face, 1.0 / 255,
+				(96, 96), (0, 0, 0), swapRB=True, crop=False)
+			embedder.setInput(faceBlob)
+			vec = embedder.forward()
+
+			# add the name of the person + corresponding face
+			# embedding to their respective lists
+			knownNames.append(name)
+			knownEmbeddings.append(vec.flatten())
+			total += 1
+
+# dump the facial embeddings + names to disk
+data = {"embeddings": knownEmbeddings, "names": knownNames}
+f = open('C:\\Users\\kkkkkkkkkkk\\Downloads\\opencv-face-recognition\\extract_embeddings.py', "wb")
+#dump the upfdated  embeddings
+f.write(pickle.dumps(data))
+f.close()
+
+
+
+from sklearn.preprocessing import LabelEncoder
+from sklearn.svm import SVC
+import pickle
+
+#load the face detector model
+detector = cv2.dnn.readNetFromCaffe(protoPath, modelPath)
+ 
+# load our face embedding model
+embedder = cv2.dnn.readNetFromTorch('C:\\Users\\kkkkkkkkkkk\\Downloads\\opencv-face-recognition\\openface_nn4.small2.v1.t7')
+ 
+# encode the labels
+le = LabelEncoder()
+labels = le.fit_transform(data["names"])
+
+# We used Support vector classification model
+recognizer = SVC(C=1.0, kernel="linear", probability=True)
+recognizer.fit(data["embeddings"], labels)
+
+f = open('C:\\Users\\kkkkkkkkkkk\\Downloads\\opencv-face-recognition\\output\\recognizer.pickle', "wb")
+f.write(pickle.dumps(recognizer))
+f.close()
+ 
+# write the label encoder to disk
+f = open('C:\\Users\\kkkkkkkkkkk\\Downloads\\opencv-face-recognition\\output\\le.pickle', "wb")
+f.write(pickle.dumps(le))
+f.close()
+
+# load the actual face recognition model along with the label encoder
+recognizer = pickle.loads(open('C:\\Users\\kkkkkkkkkkk\\Downloads\\opencv-face-recognition\\output\\recognizer.pickle', "rb").read())
+le = pickle.loads(open('C:\\Users\\kkkkkkkkkkk\\Downloads\\opencv-face-recognition\\output\\le.pickle', "rb").read())
+
+image = cv2.imread(imagePaths[3])
+image = imutils.resize(image, width=600)
+(h, w) = image.shape[:2]
+ 
+# construct a blob from the image
+imageBlob = cv2.dnn.blobFromImage(
+	cv2.resize(image, (300, 300)), 1.0, (300, 300),
+	(104.0, 177.0, 123.0), swapRB=False, crop=False)
+ 
+# apply OpenCV's deep learning-based face detector to localize faces in the input image
+detector.setInput(imageBlob)
+detections = detector.forward()
+
+for i in range(0, detections.shape[2]):
+	# extract the confidence (i.e., probability) associated with the rediction
+	confidence = detections[0, 0, i, 2]
+
+	# filter out weak detections
+	if confidence > 0.6:
+		# compute the (x, y)-coordinates of the bounding box for the face
+		box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+		(startX, startY, endX, endY) = box.astype("int")
+
+		# extract the face ROI
+		face = image[startY:endY, startX:endX]
+		(fH, fW) = face.shape[:2]
+
+		# ensure the face width and height are sufficiently large
+		if fW < 20 or fH < 20:
+			continue
+
+		# construct a blob for the face ROI, then pass the blob through our face embedding model to obtain the 128-d quantification of the face
+		faceBlob = cv2.dnn.blobFromImage(face, 1.0 / 255, (96, 96),
+			(0, 0, 0), swapRB=True, crop=False)
+		embedder.setInput(faceBlob)
+		vec = embedder.forward()
+
+		# perform classification to recognize the face
+		preds = recognizer.predict_proba(vec)[0]
+        #then it calculates the argument of maximum value of the predictions
+		j = np.argmax(preds)
+		proba = preds[j]
+		name = le.classes_[j]
+
+		# draw the bounding box of the face along with the associated
+		# probability
+		text = "{}: {:.2f}%".format(name, proba * 100)
+		y = startY - 10 if startY - 10 > 10 else startY + 10
+		cv2.rectangle(image, (startX, startY), (endX, endY),
+			(0, 0, 255), 2)
+		cv2.putText(image, text, (startX, y),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+     
+# show the output image with face recognized
+cv2.imshow("Image", image)
+cv2.waitKey(0)
+
+
+
+
+
+
+
+
